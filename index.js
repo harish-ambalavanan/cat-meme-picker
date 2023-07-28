fdaf10e26f5323f633921c7ecb8c499b8b22fc12
Harish <harishihari7@gmail.com>
2023-07-28 15:43:48 +0530

    Basic HTML and CSS setup done

---
+++ index.js
@@ -0,0 +1,144 @@
+import { catsData } from "./data.js"
+// console.log(catsData);
+const emotionRadiosDiv = document.getElementById('emotion-radios')
+const isGifOnly = document.getElementById('gifs-only-option')
+const getImageBtn = document.getElementById('get-image-btn')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getEmotionsArray(cats){
+//     const emotionsArray = []
+//     for (let cat of cats){
+//         for (let emotion of cat.emotionTags){
+//             emotionsArray.push(emotion)
+//         }
+//     }
+//     return emotionsArray
+// }
+
+
+// function renderEmotionsRadios(cats){
+//     const emotions = getEmotionsArray(cats)
+//     console.log(emotions)
+//     let emotionsData = ""
+//     for(let emotion of emotions){
+//         emotionsData += `
+//         <div class="radio">
+//             <label for="${emotion}">${emotion}<label/>
+//             <input type="radio" name="emotion-choice" id="${emotion}" value="${emotion}">
+//         </div>
+//         `
+//     }
+//     emotionRadiosDiv.innerHTML = emotionsData
+// }
+
+// renderEmotionsRadios(catsData)
